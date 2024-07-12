af113cece591e7cb00f3006e11716f9260b9a397
Piotr Rojek <128423791+PiotrasX@users.noreply.github.com>
2024-07-12 09:47:39 +0200

    Delete directory

--- Lab4/zadanie2.py
+++
@@ -1,5 +0,0 @@
-def lista_kwadratow(x):
-    return list(map(lambda y: y**2, range(1, x+1)))
-
-
-print(lista_kwadratow(5))
--- Lab4/zadanie3.py
+++
@@ -1,2 +0,0 @@
-lista = [1, 2, 3, 5, 8, 3, 0, 7]
-print(list(filter(lambda x: x > 4, lista)))

48bba0470701469f28b081a59fc93fa2dc1d2c58
Piotr Rojek <128423791+PiotrasX@users.noreply.github.com>
2024-07-12 09:47:32 +0200

    Delete directory

--- Lab5/zadanie2.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    def __init__(self, imie, numer_albumu):
-        self.imie = imie
-        self.numer_albumu = numer_albumu
-
-
-student1 = Student("Maks", 125155)
-student3 = Student("Łukasz", 125157)
-student2 = Student("Piotr", 125159)
